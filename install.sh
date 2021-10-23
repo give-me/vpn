99acce946d94a419432b02b051962c9d167e0e89
nobody <nedurak@gmail.com>
2021-10-23 10:50:27 +0300

    Add a possibility to update this tool to the latest version and improve instructions

--- install.sh
+++ install.sh
@@ -4,14 +4,24 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-info() { echo -e "\033[1;32m${1}\033[0m"; }
+info() { echo -e "\033[32m${1} \033[1;32m${2-}\033[0m"; }
+
+# Make a root directory
+ROOT="/opt/vpn-behind-outline"
+mkdir --parents "${ROOT}/bin"
+
+# Get or make numbers of public ports for Outline
+test -f "${ROOT}/ports" || cat >"${ROOT}/ports" <<EOL
+API_PORT=$((1024 + RANDOM + (RANDOM % 2) * 30000))
+KEYS_PORT=$((1024 + RANDOM + (RANDOM % 2) * 30000))
+EOL
+source "${ROOT}/ports"
 
 # Calculate interface details
-IP=$(ip route get 1.1.1.1 | awk '{print $7}' | head -1)
+DEV=$(ip route show default | awk '{print $5}')
+GW=$(ip route show default | awk '{print $3}')
+IP=$(ip route get "${GW}" | awk '{print $5}')
 CIDR=$(ip route | grep / | grep "${IP}" | awk '{print $1}')
-GATEWAY=$(ip route show 0.0.0.0/0 | awk '{print $3}')
-DEV=$(ip route show 0.0.0.0/0 | awk '{print $5}')
-info "IP: ${IP}, CIDR: ${CIDR}, GATEWAY: ${GATEWAY}, DEV: ${DEV}"
 
 # Install Docker
 which docker >/dev/null ||
@@ -23,22 +33,22 @@ which nordvpn >/dev/null ||
 
 # Install and run Outline
 PUBLIC=$(test $# -eq 1 && echo "$@" || echo "$IP")
-API_PORT=$((1024 + RANDOM + (RANDOM % 2) * 30000))
-KEYS_PORT=$((1024 + RANDOM + (RANDOM % 2) * 30000))
-bash -c "$(curl -sSL https://github.com/Jigsaw-Code/outline-server/raw/master/src/server_manager/install_scripts/install_server.sh)" \
-  install_server.sh \
-  --hostname="${PUBLIC}" \
-  --api-port="${API_PORT}" \
-  --keys-port="${KEYS_PORT}"
+docker ps | grep shadowbox >/dev/null ||
+  bash -c "$(curl -sSL https://github.com/Jigsaw-Code/outline-server/raw/master/src/server_manager/install_scripts/install_server.sh)" \
+    install_server.sh \
+    --hostname="${PUBLIC}" \
+    --api-port="${API_PORT}" \
+    --keys-port="${KEYS_PORT}"
 
 # Create a task on startup
-cat >/usr/local/bin/vpn.sh <<EOL
+cat >"${ROOT}/bin/up-vpn.sh" <<EOL
 #!/bin/sh
+export PATH=$PATH
 log() { echo "\$(date) - \${1}" >> /var/log/vpn.log; }
-log "Boot up"; sleep 10
+log "Wait some time and boot up"; sleep 10;
 # Enable BBR to improve network performance
-/usr/sbin/sysctl net.core.default_qdisc=fq
-/usr/sbin/sysctl net.ipv4.tcp_congestion_control=bbr
+sysctl net.core.default_qdisc=fq
+sysctl net.ipv4.tcp_congestion_control=bbr
 # Configure NordVPN
 nordvpn whitelist remove all
 nordvpn whitelist add port 22
@@ -53,23 +63,26 @@ log "Run NordVPN"; nordvpn connect || exit 1
 # Configure routing
 ip rule add from ${IP} table 128
 ip route add table 128 to ${CIDR} dev ${DEV}
-ip route add table 128 default via ${GATEWAY}
+ip route add table 128 default via ${GW}
 # Check health
 while :
 do
   sleep 10; ping -q -w 5 1.1.1.1 && continue
   log "Lost connection"
-  log "Try to reconnect NordVPN"; timeout 10s nordvpn connect && continue
-  log "Try to restart NordVPN's services"; \
+  log "Try to reconnect NordVPN";
+    timeout 10s nordvpn connect &&
+    log "NordVPN has successfully reconnected" &&
+    continue
+  log "Try to restart NordVPN's services";
     timeout 30s systemctl restart nordvpn.service &&
     timeout 30s systemctl restart nordvpnd.service &&
     log "The services have successfully restarted" &&
     continue
-  log "Reboot the server"; /usr/sbin/reboot --force
+  log "Reboot the server"; reboot --force
 done
 EOL
-chmod +x /usr/local/bin/vpn.sh
-echo "@reboot sh /usr/local/bin/vpn.sh >/dev/null 2>&1" | crontab -
+chmod +x "${ROOT}/bin/up-vpn.sh"
+echo "@reboot sh ${ROOT}/bin/up-vpn.sh >/dev/null 2>&1" | crontab -
 
 # TODO: Remove after fixing the bug of NordVPN 3.11.0
 # Create a temporary task to fix freezing of NordVPN
@@ -78,18 +91,32 @@ echo "@reboot sh /usr/local/bin/vpn.sh >/dev/null 2>&1" | crontab -
 # - https://forum.manjaro.org/t/nordvpn-bin-breaks-every-4-hours/80927
 # - https://aur.archlinux.org/packages/nordvpn-bin#comment-829416
 # Commands to uninstall the temporary task:
-#   rm /usr/local/bin/fix-vpn.sh
-#   echo "@reboot sh /usr/local/bin/vpn.sh >/dev/null 2>&1" | crontab -
-cat >/usr/local/bin/fix-vpn.sh <<EOL
+#   rm /opt/vpn-behind-outline/bin/fix-vpn.sh
+#   echo "@reboot sh /opt/vpn-behind-outline/bin/up-vpn.sh >/dev/null 2>&1" | crontab -
+cat >"${ROOT}/bin/fix-vpn.sh" <<EOL
 #!/bin/sh
 log() { echo "\$(date) - \${1}" >> /var/log/vpn.log; }
 log "Recreate connection"; nordvpn connect
 EOL
-chmod +x /usr/local/bin/fix-vpn.sh
+chmod +x "${ROOT}/bin/fix-vpn.sh"
 crontab -l | {
   cat
-  echo "0 */4 * * * sh /usr/local/bin/fix-vpn.sh >/dev/null 2>&1"
+  echo "0 */4 * * * sh ${ROOT}/bin/fix-vpn.sh >/dev/null 2>&1"
 } | crontab -
 
 # Notify about following actions
-info "Configure Outline Manager and run: nordvpn login && reboot"
+clear -x
+info "\nInterface ${DEV} was detected as default:"
+info "- ip:" "${IP}"
+info "- gateway:" "${GW}"
+info "- CIDR:" "${CIDR}"
+info "\nNordVPN behind Outline has been successfully configured:"
+info "- management port:" "${API_PORT} (TCP)"
+info "- access key port:" "${KEYS_PORT} (TCP and UDP)"
+info "\nPlease, do the following:"
+api=$(grep "apiUrl" "/opt/outline/access.txt" | sed "s/apiUrl://")
+cert=$(grep "certSha256" "/opt/outline/access.txt" | sed "s/certSha256://")
+secret="{\"apiUrl\":\"${api}\",\"certSha256\":\"${cert}\"}"
+info "1) Configure Outline Manager with the following string:" "${secret}"
+info "2) Run a command to log you in NordVPN:" "nordvpn login"
+info "3) Run a command to restart the server:" "reboot"
